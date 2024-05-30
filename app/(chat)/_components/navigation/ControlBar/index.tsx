cf470c8ca972c35cab371c9d637753c5be75e393
El-Gazoly <elgazoly11@gmail.com>
2024-05-30 09:33:09 +0300

    share a tab global state

--- app/(chat)/_components/navigation/ControlBar/index.tsx
+++ app/(chat)/_components/navigation/ControlBar/index.tsx
@@ -1,56 +1,26 @@
+"use client";
 import Logo from "@/assets/logo-Icon.svg";
 import ArchiveIcon from "./ArchiveIcon";
 import MessageIcon from "./MessageIcon";
 import SearchIcon from "./SearchIcon";
 import SettingsIcon from "./SettingsIcon";
 import Image from "next/image";
-import { useReducer } from "react";
 import { SignInButton, UserButton } from "@clerk/clerk-react";
- import { Authenticated, Unauthenticated } from "convex/react";
-
-type State = {
-    isArchive: boolean;
-    isMessage: boolean;
-    isSearch: boolean;
-}
-
+import { Authenticated, Unauthenticated } from "convex/react";
+import { useSelector , useDispatch} from "react-redux";
+import { RootState, AppDispatch } from "@/store/store";
+import { setTab } from "@/store/TabSlice"
 type Action = {
     type: "ARCHIVE" | "MESSAGE" | "SEARCH";
 }
 
-const reducer = (state: State, action: Action) => {
-    switch (action.type) {
-        case "ARCHIVE":
-            return {
-                isArchive: true,
-                isMessage: false,
-                isSearch: false
-            }
-        case "MESSAGE":
-            return {
-                isArchive: false,
-                isMessage: true,
-                isSearch: false
-            }
-        case "SEARCH":
-            return {
-                isArchive: false,
-                isMessage: false,
-                isSearch: true
-        }
-        default:
-            return state;
+const NavigationControlBar = () => {
+    const tab = useSelector((state: RootState) => state.tabs.tab)
+    const dispatch = useDispatch<AppDispatch>();
+    const handleTab = (tab: Action) => {
+        dispatch(setTab(tab.type))
     }
 
-}
-
-const NavigationControlBar = () => {
-    const [state, dispatch] = useReducer(reducer, {
-        isArchive: false,
-        isMessage: true,
-        isSearch: false
-    
-    });
     return ( 
         <div className=" overflow-y-hidden h-full z-10 min-w-[88px] flex flex-col  justify-between items-center py-6 bg-white"
             style={{
@@ -60,14 +30,14 @@ const NavigationControlBar = () => {
             <div className=" flex flex-col items-center gap-y-10">
                 <Image src={Logo} alt="logo" className="  w-10 h-12 lg:w-12 lg:h-14" />
                 <div className="flex flex-col items-center gap-y-10">
-                    <div className=" cursor-pointer" onClick={ () => dispatch({ type: "MESSAGE" })}>
-                        <MessageIcon color={state.isMessage ? "#0D4EC8" : "black"} />
+                    <div className=" cursor-pointer" onClick={ () => handleTab({ type: "MESSAGE" })}>
+                        <MessageIcon color={tab === "MESSAGE" ? "#0D4EC8" : "black"} />
                     </div>
-                    <div className=" cursor-pointer" onClick={ () => dispatch({ type: "ARCHIVE" })}>
-                        <ArchiveIcon color={state.isArchive ? "#0D4EC8" : "black"} />
+                    <div className=" cursor-pointer" onClick={ () => handleTab({ type: "ARCHIVE" })}>
+                        <ArchiveIcon color={tab === "ARCHIVE" ? "#0D4EC8" : "black"} />
                     </div>
-                    <div className=" cursor-pointer" onClick={ () => dispatch({ type: "SEARCH" })}>
-                        <SearchIcon   color={state.isSearch ? "#0D4EC8" : "black"} />
+                    <div className=" cursor-pointer" onClick={ () => handleTab({ type: "SEARCH" })}>
+                        <SearchIcon   color={tab === "SEARCH"? "#0D4EC8" : "black"} />
                     </div>
                 </div>
             </div>
