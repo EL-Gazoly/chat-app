a114840512d6d6772e126fd44c9f7fabbd0d031b
El-Gazoly <elgazoly11@gmail.com>
2024-05-31 18:06:00 +0300

    allow edgestore to pas

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,9 @@ import { clerkMiddleware,  createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/sign-in",
   "/sign-up",
+  "/api/edgestore/health",
+  "/api/edgestore/init"
+
 ]);
 
 export default clerkMiddleware((auth, req, next )=>{
@@ -13,5 +16,5 @@ export default clerkMiddleware((auth, req, next )=>{
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/"],
 };
